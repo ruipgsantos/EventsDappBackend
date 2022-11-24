104db7ba0497a87e5688a8d79f60d5d6b42a14e5
Rui Santos <ruipgs@gmail.com>
2022-11-24 12:08:08 +0000

    small refactoring

--- src/routes/events.ts
+++ src/routes/events.ts
@@ -9,29 +9,27 @@ const getEventsRepo = async (): Promise<EventsRepository> => {
   return (await RepositoryFactory.getInstance()).getEventsRepository();
 };
 
+/**
+ * Get All Events
+ */
 router.get("/", async (req: Request, res: Response) => {
   const eventsRepo = await getEventsRepo();
   const events = await eventsRepo.getEvents();
-
-  console.log(
-    `requested ${events ? events.length : 0} events at ${Date.now()}`
-  );
   res.send(events);
 });
 
-type SpaceParams = {
-  spaceId: number;
-};
-
-router.get("/:spaceId", async (req: Request<SpaceParams, Event[]>, res) => {
-  const eventsRepo = await getEventsRepo();
-  const events = await eventsRepo.getEventsBySpaceId(
-    Number(req.params.spaceId)
-  );
-  console.log(
-    `requested ${events ? events.length : 0} events by spaceid at ${Date.now()}`
-  );
-  res.send(events);
-});
+/**
+ * Get Events By Space Id
+ */
+router.get(
+  "/:spaceId",
+  async (req: Request<{ spaceId: number }>, res: Response<Event[]>) => {
+    const eventsRepo = await getEventsRepo();
+    const events = await eventsRepo.getEventsBySpaceId(
+      Number(req.params.spaceId)
+    );
+    res.send(events);
+  }
+);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
 import express from "express";
 import app from "./app";
-const port = 5000;
-
-
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Running on port ${port}`));
