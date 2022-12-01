3f18b20f0b437ce2b3ca977c9804a43390d63f77
Rui Santos <75912286+ruipgsantos@users.noreply.github.com>
2022-12-01 13:29:05 +0000

    Integration tests (#4)
    * integration-tests, wip
    
    * integration tests setup
    
    * trying to run with different env
    
    * run int tests
    
    * correct action for integration
    
    * testing dir ls
    
    * testing dir
    
    * testing dir 2
    
    * testing env for integration
    
    * correction
    
    * integration tests and db mig running

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  projects: [
+    "./test/unit/unit.jest.config.json",
+    "./test/integration/integration.jest.config.json",
+  ],
+  transformIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/*.json"],
+  moduleFileExtensions: ["js", "jsx", "json"],
+  globals: {
+    NODE_ENV: "test",
+  },
+};
+
+export default config;
---
+++ test/integration/events.integration.test.ts
@@ -0,0 +1,79 @@
+import request from "supertest";
+import app from "../../src/app";
+import { loadEventsData } from "../testutils";
+import { Event } from "@prisma/client";
+import DbContainer from "./db.container";
+
+import EventRepository from "../../src/db/repositories/events.repository";
+import { RepositoryFactory } from "../../src/db/repository.factory";
+import e from "express";
+
+//TODO: error scenarios
+describe("Events Routes", () => {
+  let mockEventsData: any[] = loadEventsData();
+
+  let eventRepo: EventRepository;
+
+  beforeAll(async () => {
+    mockEventsData = loadEventsData();
+    eventRepo = (await RepositoryFactory.getInstance()).getEventsRepository();
+  });
+
+  afterAll(() => {});
+
+  beforeEach(() => {
+    DbContainer.refreshDbData();
+  });
+
+  it("Returns All Events", async () => {
+    await request(app)
+      .get("/events")
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual(mockEventsData);
+      });
+  });
+
+  // it("Saves Event", async () => {
+  //   const newEvent = {
+  //     name: "newEvent",
+  //     description: "new Event Desc",
+  //     location: "new Event Location",
+  //     date: new Date(Date.now()),
+  //     spaceId: 0,
+  //   };
+
+  //   await request(app)
+  //     .post("/events")
+  //     .send(newEvent)
+  //     .expect(200)
+  //     .then((response) => {
+  //       expect(response.body).toEqual(expect.objectContaining(newEvent));
+  //     });
+
+  //   await request(app)
+  //     .get("/events")
+  //     .expect(200)
+  //     .then((response) => {
+  //       expect(response.body).toEqual(mockEventsData);
+  //     });
+  // });
+
+  //   it("Returns events array", async () => {
+  //     await request(app)
+  //       .get("/events")
+  //       .expect(200)
+  //       .then((response) => expect(response.body).toEqual(mockEventsData));
+
+  //     expect(getEventsSpy).toHaveBeenCalledTimes(1);
+  //   });
+
+  //   it("Returns events array by space id", async () => {
+  //     await request(app)
+  //       .get(`/events/${spaceId}`)
+  //       .expect(200)
+  //       .then((response) => expect(response.body).toEqual(expectedResult));
+
+  //     expect(getEventsBySpaceIdSpy).toHaveBeenCalledTimes(1);
+  //   });
+});
